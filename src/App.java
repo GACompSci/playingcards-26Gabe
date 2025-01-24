61eee9c7d96d99caed48c7dc0cad44f989a7c412
Gabriel Meinstein <gmein26@germantownacademy.org>
2025-01-23 19:10:12 -0500

    Deck Class

--- src/App.java
+++ src/App.java
@@ -1,20 +1,18 @@
 public class App {
     public static void main(String[] args) {
-      //54 cards in the deck
-        Card[] deck = new Card[54];
-        int i = 0;
-        deck[i++] = new Card();  // first joker
-        deck[i++] = new Card();  // 2nd joker
-
-        // using a nested loop to create 4 suits per card value & with 13 card values --> 52 total combinations
-        for (int value = 1; value <= 13; value++) {
-            for (int suit = 1; suit <= 4; suit++) {
-                deck[i++] = new Card(value, suit); //add it to the deck
-            }
+        Deck deck = new Deck();
+        for(Card card : deck.getDeck()){
+         //   System.out.println(card);
         }
-
-        for (Card card : deck) {// for each card in the deck
-            System.out.println(card); //print the card
-        }
+        deck.drawCard();
+        deck.drawCard();
+        System.out.println(deck);
     }
 }
+// For this exercise, you will continue to work in the Playing Cards exercise. You will create a Deck class that will define objects representing a deck of playing cards. Keep your Card class and add a new file called Deck.java. Your Deck class must include: 
+// an array of Card objects called deck (3 points)
+// a default constructor that creates and shuffles (see below) a 52 card deck (you should repurpose your nested loops form the main method here) (5 points)
+// a shuffle method that shuffles the deck array (2 points)
+// a drawCard method that returns the first card in the array and sets deck to a new, smaller array (3 points)
+// a toString method that prints "Deck of {deck.length} cards" (2 points)
+// In the main method, create a deck object, draw and print two cards, then print out the deck object. (5 points)
--- src/Card.java
+++ src/Card.java
@@ -1,18 +1,26 @@
 public class Card {
+ public static void main(String[] args) {
+     
+ }
     private int value;  // ranges 0-13 & uses 0 for joker
-    private int suit;   // Suit ranges 0-4 for hearts diamonds clubs spades & 0 forjoker
+    private final int suit;   // Suit ranges 0-4 for hearts diamonds clubs spades & 0 forjoker, I got an error message telling me to put final
     private String name;  //  name of the card 
+    private static int numCards=0;
 
+    
     public Card() { // special for jokers
         this.value = 0;
         this.suit = 0;   
         this.name = "Joker";
+        numCards++;
     }
 
     public Card(int value, int suit) {//the object is card & its attributes are value and suit. Used for everything that isn't a joker
         this.value = value;
         this.suit = suit;
-        this.name = getCardName(value, suit);  
+        this.name = generateCardName(value, this.suit);
+        numCards++;
+    //System.out.println(numCards);
     }
 
     public int getValue() {
@@ -25,18 +33,26 @@ public class Card {
 
     public void setVal(int value) {
         this.value = value;
-        this.name = getCardName(value, this.suit); 
+        this.name = generateCardName(value, suit);  
     }
 
+    public static int getNumCards(){
+        return numCards;
+    }
+@Override //  I got an error message telling me to put @ overide
     public String toString() {
-        return this.name;
+        return this.name; 
+        
+
     }
 
-    private String getCardName(int value, int suit) {
+    public String generateCardName(int value, int suit) {
         String[] cardNames = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
         String[] suitNames = {"Joker", "Hearts", "Diamonds", "Clubs", "Spades"};
         if (value == 0) {
-            return cardNames[0];}
+            return suitNames[0]; // For joker card
+        }
         return cardNames[value - 1] + " of " + suitNames[suit];
     }
+
 }
